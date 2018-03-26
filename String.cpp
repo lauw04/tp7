d83697d56f440970624f2d1c9276cb529f541835
Lauren Picard <lauren.picard@insa-lyon.fr>
2018-03-26 17:45:16 +0200

    Classe String minimale

---
+++ String.cpp
@@ -0,0 +1,11 @@
+#include "String.h"
+
+size_t String::MAX_SIZE;
+
+//Constructors/Destructor
+
+//Getters
+
+//Operators
+
+//Methodes
---
+++ String.h
@@ -0,0 +1,34 @@
+#ifndef STRING_H
+#define STRING_H
+
+class String{
+	public:
+	//Attributs
+	size_t size_;
+	char* c_str_;
+	size_t capacity_;
+	
+	protected:
+	static size_t MAX_SIZE const;	
+
+	//Constructors/Destructor
+
+
+	//Operators
+
+
+	//Methodes
+
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif
